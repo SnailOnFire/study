78b258158dcc0aba2521b20d90542d57ab43c71d
Mr.X <1252272125@qq.com>
2020-05-02 22:43:42 +0800

    PDF to image

--- back/index.js
+++ back/index.js
@@ -1,4 +1,4 @@
-pdfjsLib.GlobalWorkerOptions.workerSrc = 'https://cdn.bootcss.com/pdf.js/2.2.228/pdf.worker.min.js';
+pdfjsLib.GlobalWorkerOptions.workerSrc = '../source/js/pdf.worker.min.js';
 
 const preview = document.getElementById('preview');
 const page_num = document.getElementById('page_num');
@@ -6,10 +6,10 @@ const out_type = document.getElementById('out_type');
 
 let pdfFile, pdf, pageNum, context = preview.getContext('2d');
 
-out_type.querySelectorAll('.button').forEach(function (btn) {
+out_type.querySelectorAll('button').forEach(function (btn) {
   btn.onclick = function () {
-    out_type.querySelector('.primary').classList.remove('primary');
-    btn.classList.add('primary');
+    out_type.querySelector('.cur').classList.remove('cur');
+    btn.classList.add('cur');
   }
 });
 
@@ -62,6 +62,7 @@ function readerPage(callback) {
 
     preview.height = viewport.height;
     preview.width = viewport.width;
+    preview.style.display='block';
 
     let renderContext = {
       canvasContext: context,
@@ -76,7 +77,7 @@ function readerPage(callback) {
 function save() {
   let a = document.createElement('a');
   let event = new MouseEvent('click');
-  let type = out_type.querySelector('.primary').innerText.toLowerCase();
+  let type = out_type.querySelector('.cur').innerText.toLowerCase();
   a.download = pdfFile.name + '-' + pageNum + '.' + type;
   a.href = preview.toDataURL(type === 'png' ? 'image/png' : 'image/jpeg');
   a.dispatchEvent(event)
@@ -100,3 +101,42 @@ function savePage() {
     savePage();
   });
 }
+
+const addButtons = $('#edit_box .group');
+const processButton = $('button.process',addButtons);
+const noticeButton = $('button.notice',addButtons);
+const editMask = $('#edit_box .mask');
+const editBoxTop = $('.show').get(0).offsetTop + 30;
+var scrollTop;
+
+
+processButton.click(function(e) {
+  e.stopPropagation();
+  editMask.css('display','block');
+  noticeButton.removeClass('active');
+  $(this).addClass('active');
+})
+
+noticeButton.click(function(e) {
+  e.stopPropagation();
+  editMask.css('display','block');
+  processButton.removeClass('active');
+  $(this).addClass('active');
+})
+
+editMask.click(function() {
+  $(this).css('display','none');
+  processButton.removeClass('active');
+  noticeButton.removeClass('active');
+})
+
+$(window).scroll(function () {
+  window.pageYOffset !== undefined?scrollTop = window.pageYOffset:scrollTop = document.documentElement.scrollTop;
+  if(scrollTop > editBoxTop) {
+    addButtons.addClass('fixed');
+    addButtons.css('right', ($('body').get(0).offsetWidth - parseInt(editMask.css('width'))) / 2 + 'px');
+  } else {
+    addButtons.removeClass('fixed');
+    addButtons.css('right', 'auto');
+  }
+})
